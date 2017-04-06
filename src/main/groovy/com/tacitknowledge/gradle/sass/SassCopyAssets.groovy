1f7f95a9f293ccd6f0e4c3ce93f97682802ed557
Hyun Jeong <hjeong@tacitknowledge.com>
2017-04-06 08:09:54 -0700

    fix files not getting copied correctly from the current project

--- src/main/groovy/com/tacitknowledge/gradle/sass/SassCopyAssets.groovy
+++ src/main/groovy/com/tacitknowledge/gradle/sass/SassCopyAssets.groovy
@@ -1,19 +1,28 @@
 package com.tacitknowledge.gradle.sass
 
 import org.gradle.api.tasks.Copy
+import org.gradle.api.file.FileCopyDetails
+import org.gradle.api.file.RelativePath
 
 class SassCopyAssets extends Copy
 {
   SassCopyAssets()
   {
     project.afterEvaluate{
+      //copying scss assets from current project
+      from("${project.projectDir}/grails-app/assets/scss") {
+        include '**/*'
+        eachFile { FileCopyDetails fcp ->
+            fcp.relativePath = new RelativePath(true, 'META-INF','assets', *fcp.relativePath.segments)
+        }
+      }
+      //copying scss assets from dependencies
       from project.configurations.compile.collect {
-        project.zipTree(it).matching { include '**/META-INF/assets/**/*.scss' }
+        project.zipTree(it).matching {
+          include '**/META-INF/assets/**/*.scss'
+        }
       }
 
-      from("$project.buildDir/resources/main/") {
-        include '**/META-INF/assets/**/*.scss'
-      }
       into "$project.buildDir/scss"
     }
   }
